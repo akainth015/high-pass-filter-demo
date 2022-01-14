eb900da8eac8184908e9d01dabad680765b5ee19
Aanand Kainth <aanand@akainth.me>
2022-01-14 07:58:11 -0800

    Remove implemented code from main branch

--- denoiser/index.mjs
+++ denoiser/index.mjs
@@ -8,17 +8,16 @@ console.info(`Started a server on port ${port}. ws://localhost:${port}`);
 wss.on('connection', async function connection(ws) {
     const sourceConnection = new WebSocket("ws://localhost:8080");
 
-
     ws.on('message', data => {
         sourceConnection.send("retransmit pls tysm -denoiser");
     });
 
-    const alpha = 0.01;
-    let backgroundNoise = 0;
-
     sourceConnection.on("message", message => {
         const signal = parseFloat(message);
-        ws.send((signal - backgroundNoise > 0 ? 1 : 0).toString());
-        backgroundNoise = backgroundNoise * (1 - alpha) + signal * alpha;
+
+        /* TODO Replace this section with an algorithm to detect the spikes / filter out the noise! */
+        const corrected = signal > 50 ? 1 : 0;
+
+        ws.send(corrected.toString());
     });
 });
